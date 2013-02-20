e7d2db26f2a45cf2abf7d71b4b766441d0e8afcb
Sage LaTorra <sage@latorra.org>
2013-02-20 01:56:17 -0800

    Cleanup formatting

--- main.py
+++ main.py
@@ -17,28 +17,81 @@
 import webapp2
 import jinja2
 import os
-import handlers.home, handlers.auth, handlers.profile, handlers.search, handlers.monster, handlers.product
+import handlers.home
+import handlers.auth
+import handlers.profile
+import handlers.search
+import handlers.monster
+import handlers.product
 import configuration.site
 
 configuration.site.jinja_environment = jinja2.Environment(
   loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates")))
 
 # Define the app
-app = webapp2.WSGIApplication([webapp2.Route(r'/', handler=handlers.home.HomeHandler, name='home'),
-                              webapp2.Route(r'/monster/create', handler=handlers.monster.CreateHandler, name='monster.create'),
-                              webapp2.Route(r'/monster/<entity_id:[\d\w%]+>', handler=handlers.monster.ViewHandler, name='monster'),
-                              webapp2.Route(r'/monster/<entity_id:[\d\w%]+>/edit', handler=handlers.monster.EditHandler, name='monster.edit'),
-                              webapp2.Route(r'/login', handler=handlers.auth.LoginHandler, name='login'),
-                              webapp2.Route(r'/logout', handler=handlers.auth.LogoutHandler, name='logout'),
-                              webapp2.Route(r'/profile/edit', handler=handlers.auth.SetupHandler, name='profile.edit'),
-                              webapp2.Route(r'/profile', handler=handlers.profile.ProfileHandler, name='profile.me'),
-                              webapp2.Route(r'/profile/<profile_id:[\d\w%]+>', handler=handlers.profile.ProfileHandler, name='profile'),
-                              webapp2.Route(r'/profile/<profile_id:[\d\w%]+>/favorites', handler=handlers.profile.FavoritesHandler, name='favorites'),
-                              webapp2.Route(r'/profile/add/<access_code:[\d\w%]+>', handler=handlers.profile.AddAccessHandler, name='profile.add'),
-                              webapp2.Route(r'/monster/<entity_id:[\d\w%]+>/delete', handler=handlers.monster.DeleteHandler, name='monster.delete'),
-                              webapp2.Route(r'/monster/<entity_id:[\d\w%]+>/vote', handler=handlers.monster.VoteHandler, name='monster.favorite'),
-                              webapp2.Route(r'/product/create', handler=handlers.product.CreateHandler, name='product.create'),
-                              webapp2.Route(r'/product/<entity_id:[\d\w%]+>', handler=handlers.product.ViewHandler, name='product'),
-                              webapp2.Route(r'/search', handler=handlers.search.SearchHandler, name='search')],
+app = webapp2.WSGIApplication([webapp2.Route(
+                                r'/', 
+                                handler=handlers.home.HomeHandler, 
+                                name='home'),
+                              webapp2.Route(
+                                r'/monster/create', 
+                                handler=handlers.monster.CreateHandler, 
+                                name='monster.create'),
+                              webapp2.Route(
+                                r'/monster/<entity_id:[\d\w%]+>', 
+                                handler=handlers.monster.ViewHandler, 
+                                name='monster'),
+                              webapp2.Route(
+                                r'/monster/<entity_id:[\d\w%]+>/edit', 
+                                handler=handlers.monster.EditHandler, 
+                                name='monster.edit'),
+                              webapp2.Route(
+                                r'/login', 
+                                handler=handlers.auth.LoginHandler, 
+                                name='login'),
+                              webapp2.Route(
+                                r'/logout', 
+                                handler=handlers.auth.LogoutHandler, 
+                                name='logout'),
+                              webapp2.Route(
+                                r'/profile/edit', 
+                                handler=handlers.auth.SetupHandler, 
+                                name='profile.edit'),
+                              webapp2.Route(
+                                r'/profile', 
+                                handler=handlers.profile.ProfileHandler, 
+                                name='profile.me'),
+                              webapp2.Route(
+                                r'/profile/<profile_id:[\d\w%]+>', 
+                                handler=handlers.profile.ProfileHandler, 
+                                name='profile'),
+                              webapp2.Route(
+                                r'/profile/<profile_id:[\d\w%]+>/favorites', 
+                                handler=handlers.profile.FavoritesHandler, 
+                                name='favorites'),
+                              webapp2.Route(
+                                r'/profile/add/<access_code:[\d\w%]+>', 
+                                handler=handlers.profile.AddAccessHandler, 
+                                name='profile.add'),
+                              webapp2.Route(
+                                r'/monster/<entity_id:[\d\w%]+>/delete', 
+                                handler=handlers.monster.DeleteHandler, 
+                                name='monster.delete'),
+                              webapp2.Route(
+                                r'/monster/<entity_id:[\d\w%]+>/vote', 
+                                handler=handlers.monster.VoteHandler, 
+                                name='monster.favorite'),
+                              webapp2.Route(
+                                r'/product/create', 
+                                handler=handlers.product.CreateHandler, 
+                                name='product.create'),
+                              webapp2.Route(
+                                r'/product/<entity_id:[\d\w%]+>', 
+                                handler=handlers.product.ViewHandler, 
+                                name='product'),
+                              webapp2.Route(
+                                r'/search', 
+                                handler=handlers.search.SearchHandler, 
+                                name='search')],
                               debug=True)
 
