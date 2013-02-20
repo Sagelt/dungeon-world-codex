7c12c8141c6db8391ff35603a75a1db948516fe2
Sage LaTorra <sage@latorra.org>
2013-02-20 01:52:32 -0800

    Consolidate profile handlers

--- main.py
+++ main.py
@@ -17,7 +17,7 @@
 import webapp2
 import jinja2
 import os
-import handlers.home, handlers.auth, handlers.profile, handlers.search, handlers.monster, handlers.favorites, handlers.product
+import handlers.home, handlers.auth, handlers.profile, handlers.search, handlers.monster, handlers.product
 import configuration.site
 
 configuration.site.jinja_environment = jinja2.Environment(
@@ -33,7 +33,7 @@ app = webapp2.WSGIApplication([webapp2.Route(r'/', handler=handlers.home.HomeHan
                               webapp2.Route(r'/profile/edit', handler=handlers.auth.SetupHandler, name='profile.edit'),
                               webapp2.Route(r'/profile', handler=handlers.profile.ProfileHandler, name='profile.me'),
                               webapp2.Route(r'/profile/<profile_id:[\d\w%]+>', handler=handlers.profile.ProfileHandler, name='profile'),
-                              webapp2.Route(r'/profile/<profile_id:[\d\w%]+>/favorites', handler=handlers.favorites.FavoritesHandler, name='favorites'),
+                              webapp2.Route(r'/profile/<profile_id:[\d\w%]+>/favorites', handler=handlers.profile.FavoritesHandler, name='favorites'),
                               webapp2.Route(r'/profile/add/<access_code:[\d\w%]+>', handler=handlers.profile.AddAccessHandler, name='profile.add'),
                               webapp2.Route(r'/monster/<entity_id:[\d\w%]+>/delete', handler=handlers.monster.DeleteHandler, name='monster.delete'),
                               webapp2.Route(r'/monster/<entity_id:[\d\w%]+>/vote', handler=handlers.monster.VoteHandler, name='monster.favorite'),
